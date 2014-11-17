7db2607c3daff8f34881800b2f1b265ce5a0f5c2
ikravchenko <kravchenko.i.vl@gmail.com>
2014-11-17 09:23:07 +0100

    fixed tests running

--- app/src/androidTest/java/com/dorma/instancesaver/SaveTest.java
+++ app/src/androidTest/java/com/dorma/instancesaver/SaveTest.java
@@ -3,19 +3,23 @@ package com.dorma.instancesaver;
 import android.annotation.TargetApi;
 import android.os.Build;
 import android.test.ActivityInstrumentationTestCase2;
+import android.test.UiThreadTest;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class SaveTest extends ActivityInstrumentationTestCase2<MainActivity> {
 
-    public SaveTest(Class<MainActivity> activityClass) {
-        super(activityClass);
+    public SaveTest() {
+        super(MainActivity.class);
     }
 
+    @UiThreadTest
     @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
     public void testCheckTextAssignment() {
         String text = "something";
-        ((EditText)getActivity().findViewById(R.id.input)).setText(text);
+        EditText input = (EditText) getActivity().findViewById(R.id.input);
+        assertNotNull(input);
+        input.setText(text);
         getActivity().findViewById(R.id.submit).callOnClick();
         assertEquals(text, ((TextView) getActivity().findViewById(R.id.title)).getText());
     }
