8b64cc10686a829d25c470ed59fd7d36704ae88f
Dmitry Zaytsev <dmitry.zaicew@gmail.com>
2014-11-20 17:49:32 +0100

    Removed obsolete classes and tests

--- app/src/main/java/com/ikravchenko/instancesaver/MainActivity.java
+++ app/src/main/java/com/ikravchenko/instancesaver/MainActivity.java
@@ -2,7 +2,6 @@ package com.ikravchenko.instancesaver;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.os.Parcelable;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -10,26 +9,19 @@ import android.widget.TextView;
 import com.ikravchenko.library.SaveState;
 import com.ikravchenko.library.Saver;
 
-import java.util.ArrayList;
-
 public class MainActivity extends Activity {
 
     @SaveState
     private String text = "";
     @SaveState
     SimpleObject simpleObject;
-    @SaveState
-    int primitive = 0;
-    @SaveState
-    int[] primitiveArray;
-    @SaveState
-    ArrayList<Parcelable> parcelablesArrayList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
         new Saver().restore(this, savedInstanceState);
+
         final TextView title = (TextView) findViewById(R.id.title);
         title.setText(text);
         final EditText input = (EditText) findViewById(R.id.input);
